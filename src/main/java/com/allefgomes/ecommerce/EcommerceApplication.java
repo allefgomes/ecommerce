3ba620cf44e9df3c4c52685a098fa14b626f7038
Allef Gomes <allefgalmeida@gmail.com>
2020-07-10 14:39:20 -0300

    Create categories operation

--- src/main/java/com/allefgomes/ecommerce/EcommerceApplication.java
+++ src/main/java/com/allefgomes/ecommerce/EcommerceApplication.java
@@ -1,13 +1,30 @@
 package com.allefgomes.ecommerce;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcommerceApplication {
+import com.allefgomes.ecommerce.domain.Category;
+import com.allefgomes.ecommerce.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class EcommerceApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category techCategory = new Category(null, "Tech");
+		Category cookieCategory = new Category(null, "Cookie");
+		
+		categoryRepository.saveAll(Arrays.asList(techCategory, cookieCategory));
+	}
 }
