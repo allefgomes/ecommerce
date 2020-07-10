564926e67fa9a603d65228182d76dd0ef176b90f
Allef Gomes <allefgalmeida@gmail.com>
2020-07-10 15:12:37 -0300

    Product and association with category

--- src/main/java/com/allefgomes/ecommerce/EcommerceApplication.java
+++ src/main/java/com/allefgomes/ecommerce/EcommerceApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.allefgomes.ecommerce.domain.Category;
+import com.allefgomes.ecommerce.domain.Product;
 import com.allefgomes.ecommerce.repositories.CategoryRepository;
+import com.allefgomes.ecommerce.repositories.ProductRepository;
 
 @SpringBootApplication
 public class EcommerceApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
@@ -25,6 +30,21 @@ public class EcommerceApplication implements CommandLineRunner{
 		Category techCategory = new Category(null, "Tech");
 		Category cookieCategory = new Category(null, "Cookie");
 		
+		Product computerProduct = new Product(null, "Computer", 2000.00);
+		Product printerProduct = new Product(null, "Printer", 2000.00);
+		Product cookieTopProduct = new Product(null, "CookieTop", 1000.00);
+		Product broomProduct = new Product(null, "Broom", .00);
+		
+		techCategory.getProducts().addAll(Arrays.asList(computerProduct, printerProduct));
+		cookieCategory.getProducts().addAll(Arrays.asList(broomProduct, cookieTopProduct));
+		
+		computerProduct.getCategories().addAll(Arrays.asList(techCategory));
+		printerProduct.getCategories().addAll(Arrays.asList(techCategory));
+		cookieTopProduct.getCategories().addAll(Arrays.asList(cookieCategory));
+		broomProduct.getCategories().addAll(Arrays.asList(cookieCategory));
+		
 		categoryRepository.saveAll(Arrays.asList(techCategory, cookieCategory));
+		productRepository.saveAll(Arrays.asList(computerProduct, printerProduct, cookieTopProduct, broomProduct));
+		
 	}
 }
--- src/main/java/com/allefgomes/ecommerce/domain/Product.java
+++ src/main/java/com/allefgomes/ecommerce/domain/Product.java
@@ -8,35 +8,36 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Category implements Serializable {
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private Double price;
 	
-	@ManyToMany(mappedBy = "categories")
-	private List<Product> products = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name="PRODUCT_CATEGORY",
+		joinColumns = @JoinColumn(name="product_id"),
+		inverseJoinColumns = @JoinColumn(name="category_id")
+	)
+	private List<Category> categories = new ArrayList<>();
 	
-	public List<Product> getProducts() {
-		return products;
+	public List<Category> getCategories() {
+		return categories;
 	}
 
-	public void setProducts(List<Product> products) {
-		this.products = products;
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
 	}
-	
-	public Category() {}
 
-	public Category(Integer id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-	}
+	public Product() {}
 
 	public Integer getId() {
 		return id;
@@ -54,11 +55,26 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Product(Integer id, String name, Double price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
 
@@ -70,9 +86,12 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
-		if (id != other.id)
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
+	};
 }
