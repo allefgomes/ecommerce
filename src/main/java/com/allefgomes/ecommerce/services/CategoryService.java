32587b947c0c467d5323844d241b64f82563472b
Allef Gomes <allefgalmeida@gmail.com>
2020-08-31 21:42:21 -0300

    pagination of categories

--- src/main/java/com/allefgomes/ecommerce/services/CategoryService.java
+++ src/main/java/com/allefgomes/ecommerce/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.allefgomes.ecommerce.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.allefgomes.ecommerce.domain.Category;
@@ -18,10 +20,10 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repo;
 	
-	public List<Category> findAll() {
-		List<Category> categories = repo.findAll();
-		
-		return categories;
+	public Page<Category> findAll(Integer page, Integer linesPerPage, String orderBy,  String direction) {
+		 PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		 
+		 return repo.findAll(pageRequest); 
 	}
 	
 	public Category find(Integer id) {
